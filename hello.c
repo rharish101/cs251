83290cc8985966b8ddad65d46d401e0263bfb0bd
rharish101 <harish.rajagopals@gmail.com>
2018-03-01 18:55:05 +0530

    Added "microkernel" stuff

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+void microkernel_sendmsg(char *);
+
 int main()
 {
     printf("Hello World!\n");
+    microkernel_sendmsg("is more portable");
     return 0;
+}
+
+void microkernel_sendmsg(char *a)
+{
+    printf("microkernel: %s\n", a);
 }
